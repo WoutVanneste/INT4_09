ea1da3b5531a7978eb19db0f84d850ab595c379e
Jannes De Greve <jannes.de.greve@student.howest.be>
2019-06-13 16:57:07 +0200

    merge met develop

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      endpoint: "http://localhost:4000/"
+      endpoint: "/"
     };
 
     socket = socketIOClient(this.state.endpoint);
